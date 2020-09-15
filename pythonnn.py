85f0573e6da9c0fcd8b089a205c5ed29ca55c0a6
jantiks <58886410+jantiks@users.noreply.github.com>
2020-09-15 17:32:18 +0400

    second commit

--- pythonnn.py
+++ pythonnn.py
@@ -1,10 +1,11 @@
-# import pandas_datareader.data as web
-# import datetime
-# start = datetime.datetime(2018,1,1)
-# end = datetime.datetime(2020,1,1)
-# BTC = web.DataReader("FB","yahoo",start,end)
-# print(BTC.head())
+import pandas_datareader.data as web
+import datetime
+start = datetime.datetime(2018,1,1)
+end = datetime.datetime(2020,1,1)
+BTC = web.DataReader("FB","yahoo",start,end)
+print(BTC.head())
 import quandl
 import matplotlib.pyplot as plt
 btc = quandl.get("WIKI/AAPl.1")
-print(btc.head())
+btc.plot()
+plt.show()
